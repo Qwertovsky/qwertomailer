2e6f74fe56f795586a3f80ac2f2538525f63c19e
Qwertovsky <valery@qwertovsky.com>
2012-09-26 17:36:35 +0400

    up

--- src/com/qwertovsky/mailer/MessageContent.java
+++ src/com/qwertovsky/mailer/MessageContent.java
@@ -17,8 +17,6 @@ import java.util.Properties;
 import javax.activation.DataHandler;
 import javax.activation.DataSource;
 import javax.activation.FileDataSource;
-import javax.activation.MimeType;
-import javax.activation.MimetypesFileTypeMap;
 import javax.mail.Address;
 import javax.mail.BodyPart;
 import javax.mail.Message;
